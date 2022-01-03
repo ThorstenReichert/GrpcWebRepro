47136fe5c9eb98f2efe15d17501a2d3f5d895384
Thorsten Reichert <treiche@goiba.net>
2022-01-03 19:04:05 +0100

    add repro for GrpcWeb iissue

---
+++ TestClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Test;
+
+namespace StupidTestClient
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var channel = GrpcChannel.ForAddress(
+                new UriBuilder
+                {
+                    Port = 4444,
+                    Host = "localhost",
+                    Scheme = "http",
+                }.Uri,
+                new GrpcChannelOptions
+                {
+                    HttpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler())
+                });
+
+            var client = new TestService.TestServiceClient(channel);
+
+            using var call = client.TestStream(new Empty());
+            while (await call.ResponseStream.MoveNext(default))
+            {
+                Console.WriteLine("Response from server");
+            }
+        }
+    }
+}
---
+++ TestServer/TestServiceImplementation.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using Test;
+
+namespace StupidTestServer
+{
+    public class TestServiceImplementation : TestService.TestServiceBase
+    {
+        public override async Task TestStream(Empty request, IServerStreamWriter<Empty> responseStream, ServerCallContext context)
+        {
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                await responseStream.WriteAsync(new Empty());
+            }
+        }
+    }
+}
